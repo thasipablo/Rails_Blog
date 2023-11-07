e916afd7cdd01dc83fe5252f0b2a936d9cebd813
thasipablo <thasipablo@gmail.com>
2023-11-07 22:46:59 +0100

    setup posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/users/:user_id/posts', to: 'posts#index'
+  get '/users/:user_id/posts/:id', to: 'posts#show'
   get '/users', to: 'users#index'
   get '/users/:id', to: 'users#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
