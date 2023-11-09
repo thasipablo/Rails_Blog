0990da9e616ca707b1bdfd105d9d35c82fe7da3c
thasipablo <thasipablo@gmail.com>
2023-11-09 10:37:34 +0100

    add post index controller specs

--- spec/requests/post_spec.rb
+++ spec/requests/post_spec.rb
@@ -2,6 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    before :each do
+      get '/users/1/posts'
+    end
+    it 'returns success status' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders index' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'include the correct placeholder text' do
+      expect(response.body).to include('Posts#index')
+    end
   end
 end
