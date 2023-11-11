c027f23f550b58b70f6c633615db0bf5f162ddc3
thasipablo <thasipablo@gmail.com>
2023-11-11 13:48:51 +0100

    fetch and show data on views templates

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,11 @@
 class PostsController < ApplicationController
   def index
-    puts 'index'
+    @user = User.find(params[:user_id])
+    @posts = Post.where(author_id: @user.id)
   end
 
   def show
-    puts 'show'
+    @post = Post.find(params[:id])
+    @comments = @post.comments
   end
 end
