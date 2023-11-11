50cd72805aa179a47a99d58b28a77851b64bfb24
thasipablo <thasipablo@gmail.com>
2023-11-11 18:11:37 +0100

    populate the post view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,31 @@
 class PostsController < ApplicationController
+  before_action :set_user
+  before_action :set_post, only: [:show]
+
+  # Create an index action taking the user id as a parameter
   def index
-    @user = User.find(params[:user_id])
-    @posts = Post.where(author_id: @user.id)
+    @posts = @user.posts
+    @comments = []
+
+    @posts.each do |post|
+      comments = post.comments.limit(5)
+      @comments << comments
+    end
   end
 
+  # create a show action
   def show
     @post = Post.find(params[:id])
-    @comments = @post.comments
+    @author = User.find(@post.author_id)
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def set_post
+    @post = @user.posts.find(params[:id])
   end
 end
