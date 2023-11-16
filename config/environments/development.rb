35713276db836cec8baae173b6cce23a01b3d82a
thasipablo <thasipablo@gmail.com>
2023-11-16 01:44:21 +0100

    setup devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,9 +8,6 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.enable_reloading = true
 
-  # default URL options for the Devise
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
   # Do not eager load code on boot.
   config.eager_load = false
 
@@ -43,6 +40,9 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+
+  # default URL options for the Devise
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
