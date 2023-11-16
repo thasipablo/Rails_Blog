e1aabb0289b373dbe987b41ce3521a63938783f2
thasipablo <thasipablo@gmail.com>
2023-11-16 21:34:08 +0100

    allow params in the app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
-  before_action :current_user
+  before_action :allowed_parameters, if: :devise_controller?
+
+  def allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
+  end
 end
